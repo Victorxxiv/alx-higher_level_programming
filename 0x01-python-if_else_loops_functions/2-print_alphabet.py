8593f180f6fc7397e7da423255ad147f53d237c3
Victor <victormwendwa804@gmail.com>
2023-11-05 23:44:54 -0800

    Alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-# Intialize and empty string to store the alphabet
-alphabet = " "
-
-# Loop through the ASCII lowercase alphabet
-for char in range(ord('a'), ord('z') + 1):
-    alphabet += chr(char)
-
-# Print the alphabet without a new line
-print("{}".format(alphabet), end=" ")
+for character in range(97, 123):
+    print("{:c}".format(character), end='')
