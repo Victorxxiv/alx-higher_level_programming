f54915908b1b676b00ce186b883750c019471495
Victor <victormwendwa804@gmail.com>
2023-11-12 22:18:15 -0800

    Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    num_keys = len(a_dictionary)
+    return num_keys
+
+
+# Example usage
+if __name__ == "__main__":
+    a_dictionary = {'language': "C", 'number': 13, 'track': "Low level"}
+    nb_keys = number_keys(a_dictionary)
+    print("Number of keys: {:d}".format(nb_keys))
