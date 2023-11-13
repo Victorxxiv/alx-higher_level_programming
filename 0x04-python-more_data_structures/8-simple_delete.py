418dcf635ce2deabc185baa3ccba60fa8e631e23
Victor <victormwendwa804@gmail.com>
2023-11-13 00:01:20 -0800

    Delete, key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -9,7 +9,12 @@ def simple_delete(a_dictionary, key=""):
 
 # Example usage
 if __name__ == "__main__":
-    _dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
+    _dictionary = {
+            'language': "C",
+            'Number': 89,
+            'track': "Low",
+            'ids': [1, 2, 3]
+            }
 
     new_dict = simple_delete(a_dictionary, 'track')
     print_sorted_dictionary(a_dictionary)
