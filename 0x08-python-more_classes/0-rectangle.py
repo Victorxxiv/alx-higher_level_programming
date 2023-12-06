7fe53fe06cc39d6f654e2f43289bb20ebe68a31f
Victor <victormwendwa804@gmail.com>
2023-12-06 00:07:11 -0800

    Second commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle."""
+
+
+class Rectangle:
+    """
+    The is an empty class Rectangle that defines a rectangle.
+
+    Attributes:
+        No attributes at the moment.
+
+    Methods:
+        No methods at the moment.
+    """
+    pass
