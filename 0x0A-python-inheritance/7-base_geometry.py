1340730f1b45819d2e1aa6c6a1428a1b433109b6
Victor <victormwendwa804@gmail.com>
2024-01-05 10:21:36 -0800

    Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,15 +11,16 @@ class BaseGeometry:
         """
         Public instance method tat raises an exception method.
         """
+        raise Exception("area() is not implemented")
 
         def integer_validator(self, name, value):
             """
             Public instance method to validate the value.
             Args:
-                Self:
-                Name:
+                Name: Asting representing the name of the value.
                 Value: The value to be validated.
-            Returns:
             """
             if not isinstance(value, int):
-                raise TypeError
+                raise TypeError("{} must be an integer".format(name))
+            if value <= 0:
+                raise ValueError("{} must be greater than 0".format(name))
